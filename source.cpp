fe91eff3837f9812847c3aaedc5638f922224d8e
Luka <lukajanke23@gmail.com>
2023-11-26 11:36:05 +0100

    addidng and refraction DONE

--- source.cpp
+++ source.cpp
@@ -28,10 +28,11 @@ class Node{
             maxKeys = m-1;
             minKeys = m/2;
             minBranches = m/2+1;
-            branches.push_back(nullptr);
+            branches.push_back(NULL);
             leaf=root=false;
             cout << "Node created!" << endl;
         }
+        bool overFull(){return (keys.size() > maxKeys)?false:true;}
         bool isFull(){return (keys.size() < maxKeys)?false:true;}
         int getNodeSize(){return maxBranches;}
 };
@@ -44,11 +45,66 @@ bool compareNode(Node* n1,Node* n2){
     return n1->keys[0]->getValue() < n2->keys[0]->getValue();
 }
 
-// FIXME
-void addKey(Node* temp,Data* data){
+// DONE
+void refraction(Node *temp,Data* data){
+    int i;
+    Node* left,*right,*parent,*destroy;
+    do{
+        left=new Node(temp->getNodeSize());
+        right=new Node(temp->getNodeSize());
+        Data* tempData;
+
+        int middle=temp->keys.size();
+        tempData=temp->keys[middle];
+        //splitting overfull node
+        for(i=0;i<temp->keys.size();i++){
+            if(i<middle){
+                left->keys.push_back(temp->keys[i]);
+                left->branches.push_back(temp->branches[i]);
+            }
+            if(i==middle)
+                left->branches.push_back(temp->branches[i]);
+            if(i>middle){
+                right->keys.push_back(temp->keys[i]);
+                right->branches.push_back(temp->branches[i]);
+            }
+        }
+        right->branches.push_back(temp->branches[i]);
+
+        if(left->branches[0]==NULL){
+            left->leaf=right->leaf=true;
+        }
+
+        if(temp->root){
+            temp->keys.clear();
+            temp->branches.clear();
+            temp->leaf=false;
+            parent=temp;
+        }
+        else{
+            destroy=temp;
+            parent=temp->parent;
+            delete(destroy);
+        }
+        parent->keys.push_back(tempData);
+        sort(parent->keys.begin(),parent->keys.end(),compareVec);
+        parent->branches.push_back(left);
+        parent->branches.push_back(right);
+        sort(parent->branches.begin(),parent->branches.end(),compareNode);
+
+        temp=parent;
+    }while(!temp->overFull());
+}
+
+// DONE
+void addKey(Node* root,Data* data){
+    Node* temp=root;
     if(!temp->isFull() && temp->leaf){
+        if(temp->keys.empty())
+            temp->branches.push_back(NULL);
         temp->keys.push_back(data);
-        temp->branches.push_back(nullptr);
+        temp->branches.push_back(NULL);
+        sort(temp->keys.begin(),temp->keys.end(),compareVec);
     }
     else {
         while(!temp->leaf){
@@ -58,100 +114,27 @@ void addKey(Node* temp,Data* data){
             else if( i == temp->keys.size()-1 && temp->keys[i]->getValue() < data->getValue())
                 temp=temp->branches[i+1];
         }
-        if(!temp->isFull() && temp->leaf){
+        
         temp->keys.push_back(data);
-        temp->branches.push_back(nullptr);
-        }
-        else refractionLeaf(temp,data);
+        temp->branches.push_back(NULL);
+        sort(temp->keys.begin(),temp->keys.end(),compareVec);
+
+        if(temp->overFull())
+            refraction(temp,data);
     }
-    sort(temp->keys.begin(),temp->keys.end(),compareVec);
 }
 
-// FIXME
-void refractionLeaf(Node *temp,Data* data){
-    Node* left,*right,*parent,*destroy;
-    vector<Data*> datas;
-    //get keys align and find middle one
-    for(int i=0; i<temp->keys.size();i++)datas.push_back(temp->keys[i]);
-    datas.push_back(data);
-    sort(temp->keys.begin(),temp->keys.end(),compareVec);
-    int middle=datas.size()/2;
-    //creating left and right node
-    left=new Node(temp->getNodeSize());
-    left->leaf=true;
-    right=new Node(temp->getNodeSize());
-    right->leaf=true;
-    for(int i=0;i<datas.size();i++){
-        if(i<middle){
-            left->keys.push_back(datas[i]);
-            left->branches.push_back(nullptr);
-        }
-        if(i>middle){
-            right->keys.push_back(datas[i]);
-            right->branches.push_back(nullptr);
-        }
-    }
+// TODO
+void deleteKey(){
 
-    if(temp->root){
-        temp->keys.clear();
-        temp->branches.clear();
-        parent=temp;
-    }
-    else{//FIXME    Dealocate temp
-        destroy=temp;
-        parent=temp->parent;
-    }
-
-    parent->keys.push_back(datas[middle]);
-    sort(parent->keys.begin(),parent->keys.end(),compareVec);
-    parent->branches.push_back(left);
-    parent->branches.push_back(right);
-    sort(parent->branches.begin(),parent->branches.end(),compareNode);
-    // refractionNode
-    if(parent->isFull())
-        refractionNode(parent);
 }
 
-// FIXME need to return pointer on root
-void refractionNode(Node* temp){
-    do{
-        Node* parent,*destroy;
-        Node* left=new Node(temp->getNodeSize());
-        Node* right=new Node(temp->getNodeSize());
-        Data* tempData;
+// TODO
+void compression(){
 
-        int middle=temp->keys.size();
-        tempData=temp->keys[middle];
-        for(int i=0;i<temp->keys.size();i++){
-        if(i<middle){
-            left->keys.push_back(temp->keys[i]);
-            left->branches.push_back(nullptr);
-        }
-        if(i>middle){
-            right->keys.push_back(temp->keys[i]);
-            right->branches.push_back(nullptr);
-        }
-
-        if(temp->root){
-            temp->keys.clear();
-            temp->branches.clear();
-            parent=temp;
-        }
-        else{//FIXME    Dealocate temp
-            destroy=temp;
-            parent=temp->parent;
-        }
-        parent->keys.push_back(tempData);
-        sort(parent->keys.begin(),parent->keys.end(),compareVec);
-        parent->branches.push_back(left);
-        parent->branches.push_back(right);
-        sort(parent->branches.begin(),parent->branches.end(),compareNode);
-
-        temp=parent;
-    }
-    }while(!temp->isFull());
 }
-//TODO test adding and writting
+
+// TODO test adding and writting
 int main(){
     int m = 0;
     Data* dat;
@@ -161,5 +144,7 @@ int main(){
         if(m < 3 || m > 10) cout << "Node size is out of range. Size should be between 3 and 10." << endl;
     }
     Node* tree = new Node(m);
+    tree->root=tree->leaf=true;
+    cout << tree->branches[0]<< " " << tree->branches[1] << endl;
     return 0;
 }
